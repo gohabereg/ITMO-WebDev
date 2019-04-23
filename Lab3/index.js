649662a1e517f3e48557bedb9c2a1b6d4c581096
georgyb <yegor.berezhnoi@3d4medical.com>
2019-04-23 19:12:08 +0300

    Lab 3 initial

---
+++ Lab3/index.js
@@ -0,0 +1,148 @@
+class CoverGenerator {
+  constructor() {
+    this.height = 800;
+    this.width = 800;
+    this.fontSize = 30;
+    this.lineHeight = 1.5 * this.fontSize;
+
+    this.canvas = this.createElement('canvas', undefined, {width: this.width, height: this.height});
+    this.context = this.canvas.getContext('2d');
+    this.context.font = `${this.fontSize}px "Times New Roman"`;
+    this.context.textBaseline = 'bottom';
+    this.context.fillStyle = '#fff';
+
+    document.body.appendChild(this.canvas);
+
+    this.generate();
+  }
+
+  async generate() {
+    this.context.clearRect(0, 0, this.width, this.height);
+
+    await this.addImages();
+    this.addOverlay();
+    await this.addQuote();
+  }
+
+  async addImages() {
+    const images = await Promise.all(this.fetchImages());
+
+    images.forEach((img, i) => {
+      this.context.drawImage(img, i % 2 * this.width / 2, Math.floor(i / 2) * this.height / 2);
+    });
+  }
+
+  addOverlay() {
+    const oldStyle = this.context.fillStyle;
+
+    this.context.fillStyle = 'rgba(0, 0, 0, 0.65)';
+    this.context.fillRect(0, 0, this.width, this.height);
+
+    this.context.fillStyle = oldStyle;
+  }
+
+  async addQuote() {
+    const quote = await this.fetchQuote();
+
+    const words = quote.split(' ');
+    const padding = 0.2 * this.width;
+
+    let currentLine = '';
+
+    console.log(quote);
+
+    const lines = words.reduce((result, word, i) => {
+
+      const {width} = this.context.measureText(`${currentLine} ${word}`);
+
+      if (width >= this.width - 2 * padding || i === words.length - 1) {
+        result.push(currentLine.trim());
+
+        currentLine = word;
+      } else {
+        currentLine += ` ${word}`;
+      }
+
+      return result;
+    }, []);
+
+    console.log(lines);
+
+    lines.forEach((line, i) => {
+
+      const {width} = this.context.measureText(line);
+
+      const x = (this.width - width) / 2;
+      const y = (this.height - lines.length * this.lineHeight) / 2 + (i + 1) * this.lineHeight;
+
+      this.context.fillText(line, x, y);
+    });
+  }
+
+  fetchImages() {
+    const endpoint = `https://source.unsplash.com/collection/1111680/${this.width / 2}x${this.height / 2}`;
+    const promises = [];
+
+    for (let i = 0; i < 4; i++) {
+      const image = new Image();
+
+      promises.push(new Promise(resolve => {
+        image.addEventListener('load', () => {
+          resolve(image);
+        })
+      }))
+
+      image.src = `${endpoint}?${Math.random()}`;
+    }
+
+    return promises;
+  }
+
+  async fetchQuote() {
+    const endpoint = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=ru&jsonp=receiveQuote';
+
+    return new Promise(resolve => {
+      let quoteText;
+
+      window.receiveQuote = (quote) => {
+        quoteText = quote.quoteText;
+      }
+      const script = this.createElement('script', undefined, {src: endpoint});
+
+      script.addEventListener('load', () => {
+        window.receiveQuote = null;
+        script.remove();
+
+        resolve(quoteText);
+      });
+
+      document.head.appendChild(script);
+    });
+  }
+
+  createElement(name, classList, attributes) {
+    const element = document.createElement(name);
+
+    if (classList) {
+      if (typeof classList === 'string') {
+        element.classList.add(classList);
+      } else {
+        element.classList.add(...classList);
+      }
+    }
+
+    if (attributes) {
+      Object
+        .entries(attributes)
+        .forEach(([name, value]) => {
+          element.setAttribute(name, value);
+        })
+    }
+
+    return element;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  new CoverGenerator();
+})
